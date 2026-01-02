37e776f17d4b95ee50f50ee79cd15e5b2132a0f4
Safa Kelkit <safakelkithms@gmail.com>
2026-01-02 21:06:18 +0300

    .

---
+++ calculating_pi2.c
@@ -0,0 +1,49 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static long steps = 100000000;
+double step;
+
+int main(int argc, const char* argv[]) {
+
+    printf("omp_get_max_threads() = %d\n", omp_get_max_threads());
+
+    int thread_counts[] = {1, 2, 4, 8, 16};
+    int num_cases = sizeof(thread_counts) / sizeof(thread_counts[0]);
+
+    step = 1.0 / (double)steps;
+
+    for (int tc = 0; tc < num_cases; ++tc) {
+        int nthreads = thread_counts[tc];
+        omp_set_num_threads(nthreads);
+
+        double start_over = omp_get_wtime();
+        #pragma omp parallel
+        {
+            #pragma omp for simd
+            for (long i = 0; i < steps; ++i) {
+                double x = (i + 0.5) * step;
+            }
+        }
+
+        double start = omp_get_wtime();
+
+        double sum = 0.0;
+        #pragma omp parallel
+        {
+            #pragma omp for simd reduction(+:sum)
+            for (long i = 0; i < steps; ++i) {
+                double x = (i + 0.5) * step;
+                sum += 4.0 / (1.0 + x * x);
+            }
+        }
+
+        double pi = sum * step;
+        double delta = omp_get_wtime() - start;
+
+        printf("PI = %.16g computed in %.4g seconds with %d threads.\n", pi, delta, nthreads);    
+    }
+
+    return 0;
+}
---
+++ matrix_vector_multiplication.c
@@ -0,0 +1,54 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    int n = 16000;                
+    if (argc >= 2) n = atoi(argv[1]);
+
+    double *A = (double*)malloc((size_t)n * n * sizeof(double));
+    double *x = (double*)malloc((size_t)n * sizeof(double));
+    double *y = (double*)malloc((size_t)n * sizeof(double));
+
+    for (int j = 0; j < n; ++j)
+        x[j] = 1.0;
+
+    for (int i = 0; i < n; ++i) {
+        int row = i * n;
+        for (int j = 0; j < n; ++j) {
+            A[row + j] = 1.0;
+        }
+    }
+
+    int thread_counts[] = {1, 2, 4, 8};
+    int num_cases = sizeof(thread_counts) / sizeof(thread_counts[0]);
+
+    for (int tc = 0; tc < num_cases; ++tc) {
+        int nthreads = thread_counts[tc];
+        omp_set_num_threads(nthreads);
+
+        double start = omp_get_wtime();
+
+        #pragma omp parallel for
+        for (int i = 0; i < n; ++i) {
+            double sum = 0.0;
+            int row = i * n;
+
+            for (int j = 0; j < n; ++j) {
+                sum += A[row + j] * x[j];
+            }
+            y[i] = sum;
+        }
+
+        double elapsed = omp_get_wtime() - start;
+
+        printf("threads = %d, time = %.4f seconds\n",
+               nthreads, elapsed);
+    }
+
+    free(A);
+    free(x);
+    free(y);
+
+    return 0;
+}
