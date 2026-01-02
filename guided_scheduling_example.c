4e6a0923009a7c2f85f394d8a73d00b17c9b8dbb
Safa Kelkit <safakelkithms@gmail.com>
2026-01-03 02:32:43 +0300

    guided_scheduling

---
+++ guided_scheduling_example.c
@@ -0,0 +1,11 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    #pragma omp parallel for schedule(guided, 3)
+    for(int i = 0; i < 40; i++){
+        printf("Guided Scheduling - Thread %d is working on iteration %d\n", omp_get_thread_num(), i);
+    }
+    return 0;
+}
